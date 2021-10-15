33f10f721dcc9132593dcbe2ecbc9dc1133de15d
Marcoseven <marco.caliciotti3@gmail.com>
2021-10-15 09:18:58 +0200

    Aggiunte modifiche - dadi
    Inseriti i commenti, i console.log e un condizionale if

--- dadi/assets/js/main.js
+++ dadi/assets/js/main.js
@@ -3,6 +3,22 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. 
 */
 
+// variabili
 const numUser = prompt("Inserire un numero compreso tra 1 a 6");
-const numbers = Math.floor(Math.random() * 7);
-console.log(numbers);
+const numComputer = Math.floor(Math.random() * 7);
+
+// console.log
+console.log(numUser);
+console.log(numComputer);
+
+// condizionale if
+if (numUser > numComputer) {
+	console.log("Hai vinto!");
+	document.getElementById("result").innerHTML = "Hai vinto!";
+} else if (numUser < numComputer) {
+	console.log("Hai perso, mi dispiace");
+	document.getElementById("result").innerHTML = "Hai perso, mi dispiace";
+} else if (numUser == numComputer) {
+	console.log("Punteggio pari");
+	document.getElementById("result").innerHTML = "Punteggio pari";
+}
